6695a24e88d149e5aa2bf2777190844f7e0bb7f1
Yan Naung (Raymond) Maung <ymaung@woolworths.com.au>
2018-11-02 12:18:52 +1100

    Init

---
+++ AsyncToSyncDeadLockDemo/Controllers/AsyncToSync.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncToSyncDeadLockDemo.Controllers
+{
+    public class AsyncToSync
+    {
+        public static string GetString()
+        {
+            var url = "https://blog.stephencleary.com/2012/07/dont-block-on-async-code.html";
+            using (var client = new WebClient())
+            {
+                string html = client.DownloadString(url);
+                return html;
+            }
+        }
+
+        public static async Task<string> GetStringAsync()
+        {
+            var url = "https://blog.stephencleary.com/2012/07/dont-block-on-async-code.html";
+            using (var client = new HttpClient())
+            {
+                var html = await client.GetStringAsync(new Uri(url));
+                return html;
+            }
+        }
+    }
+}
---
+++ AsyncToSyncDeadLockDemo/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AsyncToSyncDeadLockDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Log.Info("Index Page Requested ===>");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+
+        }
+
+
+        public ActionResult SyncPage()
+        {
+            ViewBag.Message = "Sync Page Here";
+
+            MyPage myPage = new MyPage
+            {
+                Content = AsyncToSync.GetString()
+            };
+
+            ViewBag.MyPage = myPage;
+
+            return View();
+        }
+
+
+        public async Task<ActionResult> AsyncPage()
+        {
+            ViewBag.Message = "Async Page Here";
+
+            MyPage myPage = new MyPage
+            {
+                Content = await AsyncToSync.GetStringAsync()
+            };
+
+            ViewBag.MyPage = myPage;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class MyPage
+    {
+        public string Content { get; set; }
+    }
+}
