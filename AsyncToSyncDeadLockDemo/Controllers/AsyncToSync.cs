c681b5ac6e0cbdc4ea93e025b2ba48dab0954a87
Raymond Maung <raymaung@gmail.com>
2018-11-07 21:55:48 +1100

    Add QWIPage

--- AsyncToSyncDeadLockDemo/Controllers/AsyncToSync.cs
+++ AsyncToSyncDeadLockDemo/Controllers/AsyncToSync.cs
@@ -2,7 +2,9 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Hosting;
 
 namespace AsyncToSyncDeadLockDemo.Controllers
 {
@@ -14,17 +16,19 @@ namespace AsyncToSyncDeadLockDemo.Controllers
             using (var client = new WebClient())
             {
                 string html = client.DownloadString(url);
-                return html;
+                return html.Length.ToString();
             }
         }
 
-        public static async Task<string> GetStringAsync()
+        public static async Task<string> GetStringAsync(int duration = 1000)
         {
+            Log.Info($"GetStringAsync.duration = #{duration}");
             var url = "https://blog.stephencleary.com/2012/07/dont-block-on-async-code.html";
             using (var client = new HttpClient())
             {
+                Thread.Sleep(duration);
                 var html = await client.GetStringAsync(new Uri(url));
-                return html;
+                return html.Length.ToString();
             }
         }
     }
--- AsyncToSyncDeadLockDemo/Controllers/HomeController.cs
+++ AsyncToSyncDeadLockDemo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace AsyncToSyncDeadLockDemo.Controllers
@@ -36,11 +38,48 @@ namespace AsyncToSyncDeadLockDemo.Controllers
 
         public async Task<ActionResult> AsyncPage()
         {
+            Log.Info("AsyncPage ==>");
             ViewBag.Message = "Async Page Here";
 
             MyPage myPage = new MyPage
             {
                 Content = await AsyncToSync.GetStringAsync()
+            };
+
+            ViewBag.MyPage = myPage;
+
+            return View();
+        }
+
+
+        public ActionResult QBWIPage()
+        {
+            Log.Info("QBQIPage ==>");
+            ViewBag.Message = "QBWI Page Here";
+
+            
+            HostingEnvironment.QueueBackgroundWorkItem(cancellationToken =>
+            {
+                try
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        var duration = i * 1000;
+                        AsyncToSync.GetStringAsync(duration)
+                            .ContinueWith(t => { Log.Info($"QBWI - StringAsync Completed duration: #{duration}"); }, cancellationToken);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Info("QueueBackgroundWorkItem - Exception", e);
+                }
+            });
+
+            MyPage myPage = new MyPage
+            {
+                Content = "QBWI - Page Here"
             };
 
             ViewBag.MyPage = myPage;
